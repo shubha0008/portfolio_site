f05d21bff52bdcb5a9d512052db2726d2bc90fce
shubha0008 <shubharashmi101@gmail.com>
2020-08-20 23:13:03 +0530

    Updates

---
+++ precache-manifest.00a64ab9331b1e280f807a826f710951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9c9a9bd64305fad448cf72a29708d0",
+    "url": "/portfolio_site/index.html"
+  },
+  {
+    "revision": "ddba938a5511d0530c28",
+    "url": "/portfolio_site/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "165d5e3f523d517b8d0c",
+    "url": "/portfolio_site/static/css/main.525d6571.chunk.css"
+  },
+  {
+    "revision": "ddba938a5511d0530c28",
+    "url": "/portfolio_site/static/js/2.7b4cd5d6.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio_site/static/js/2.7b4cd5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165d5e3f523d517b8d0c",
+    "url": "/portfolio_site/static/js/main.79494d58.chunk.js"
+  },
+  {
+    "revision": "24827457f2177d1c9b27",
+    "url": "/portfolio_site/static/js/runtime-main.330e7c76.js"
+  }
+]);
